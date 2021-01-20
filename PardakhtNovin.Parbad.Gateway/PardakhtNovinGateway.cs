cdad1b060400cfd11d042ff01738fb3e6f94d579
Masoud DaneshPour <masoud.net@hotmail.com>
2021-01-20 14:17:39 +0330

    add Gateway config file

---
+++ PardakhtNovin.Parbad.Gateway/PardakhtNovinGateway.cs
@@ -0,0 +1,30 @@
+﻿namespace PardakhtNovin.Parbad.Gateway
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using global::Parbad;
+    using global::Parbad.Abstraction;
+    using global::Parbad.GatewayBuilders;
+
+    public class PardakhtNovinGateway : GatewayBase<PardakhtNovinGatewayAccount>
+    {
+        public const string Name = "PardakhtNovin";
+        public PardakhtNovinGateway(IGatewayAccountProvider<PardakhtNovinGatewayAccount> accountProvider) : base(accountProvider)
+        { }
+
+        public override Task<IPaymentRequestResult> RequestAsync(Invoice invoice, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<IPaymentVerifyResult> VerifyAsync(InvoiceContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<IPaymentRefundResult> RefundAsync(InvoiceContext context, Money amount, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
