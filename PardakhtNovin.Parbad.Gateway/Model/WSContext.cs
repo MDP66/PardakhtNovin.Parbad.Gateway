3789ba5789ff7267590d8124484a6144e82a0970
Masoud DaneshPour <masoud.net@hotmail.com>
2021-02-03 14:53:26 +0330

    add wscontext dto

---
+++ PardakhtNovin.Parbad.Gateway/Model/WSContext.cs
@@ -0,0 +1,23 @@
+﻿namespace PardakhtNovin.Parbad.Gateway.Model
+{
+    using Ardalis.GuardClauses;
+    using System;
+    using Newtonsoft.Json;
+
+    [Serializable]
+    public class WSContext
+    {
+        public WSContext(string username, string password)
+        {
+            Guard.Against.NullOrEmpty(username, nameof(username));
+            Guard.Against.NullOrEmpty(password, nameof(password));
+
+            UserId = username;
+            Password = password;
+        }
+        [JsonProperty(nameof(UserId))]
+        public string UserId { get; private set; }
+        [JsonProperty(nameof(Password))]
+        public string Password { get; private set; }
+    }
+}
