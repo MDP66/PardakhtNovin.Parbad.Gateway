65e0ef91df8b42df0c0a763649fe488882f41942
Masoud DaneshPour <masoud.net@hotmail.com>
2021-02-03 14:57:31 +0330

    Add Pardakht novin api urls

---
+++ PardakhtNovin.Parbad.Gateway/PardakhtNovinGatewayOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace PardakhtNovin.Parbad.Gateway
+{
+    public class PardakhtNovinGatewayOptions
+    {
+        public string ApiLoginUrl { get; set; } = "https://pna.shaparak.ir/ref-payment2/RestServices/mts/merchantLogin/";
+        public string PaymentPageUrl { get; set; } = "https://pna.shaparak.ir/_ipgw_/payment/";
+
+        public string ApiTransactionDataToSignUrl { get; set; } = "https://pna.shaparak.ir/ref-payment2/RestServices/mts/generateTransactionDataToSign/";
+        public string ApiGenerateTokenUrl { get; set; } = "https://pna.shaparak.ir/ref-payment2/RestServices/mts/generateSignedDataToken/";
+
+        public string ApiVerificationUrl { get; set; } = "https://pna.shaparak.ir/ref-payment2/RestServices/mts/verifyMerchantTrans/";
+
+        public string ApiRefundUrl { get; set; } = "https://pna.shaparak.ir/ref-payment2/RestServices/mts/reverseMerchantTrans/";
+    }
+}
