2245eb6a7886651a328c0df9ad1fa6c777072e09
Masoud DaneshPour <masoud.net@hotmail.com>
2021-02-03 14:58:00 +0330

    Create Transaction types

--- PardakhtNovin.Parbad.Gateway/PardakhtNovinGatewayAccount.cs
+++ PardakhtNovin.Parbad.Gateway/PardakhtNovinGatewayAccount.cs
@@ -4,5 +4,10 @@
 
     public class PardakhtNovinGatewayAccount : GatewayAccount
     {
+        public long TerminalId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
     }
 }
