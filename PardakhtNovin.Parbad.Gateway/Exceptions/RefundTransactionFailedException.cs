d388a3be61e9c8d7de411006119d89e580de2d70
Masoud DaneshPour <masoud.net@hotmail.com>
2021-02-14 10:16:21 +0330

    add refund faild exception

---
+++ PardakhtNovin.Parbad.Gateway/Exceptions/RefundTransactionFailedException.cs
@@ -0,0 +1,11 @@
+﻿namespace PardakhtNovin.Parbad.Gateway.Exceptions
+{
+    using System;
+    using global::Parbad.Options;
+
+    public class RefundTransactionFailedException : Exception
+    {
+        public RefundTransactionFailedException(MessagesOptions messagesOptions) : base(messagesOptions.UnexpectedErrorText)
+        { }
+    }
+}
